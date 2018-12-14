00517a913ca815ea57dd74c40328d6974ee22804
Alec Dusheck <a.dusheckirl@gmail.com>
2018-12-13 21:24:11 -0600

    Properly log user out when 401 is returned from api

--- src/app/core/apiinterceptor.ts
+++ src/app/core/apiinterceptor.ts
@@ -3,17 +3,27 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import {AuthenticationService} from "./services/authentication.service";
+import {SelectedServerService} from './services/selected-server.service';
 
 
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
-  constructor(private auth: AuthenticationService) {}
+  constructor(private auth: AuthenticationService, private selectedServer: SelectedServerService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
       if (err.status === 401) {
         // auto logout if 401 response returned from api
-        this.auth.logout();
+
+        //TODO: remove repeated code
+
+        // remove user from local storage to log user out
+        localStorage.removeItem('session');
+        this.auth.clearUser();
+
+        //Make sure we clear the server on logout
+        this.selectedServer.servers = undefined;
+        this.selectedServer.setCurrentServer(undefined, false);
         location.reload(true);
       }
 
