678f839df96ff15ec9897b1c2f6b8a2cc2513798
Alec Dusheck <a.dusheckirl@gmail.com>
2018-12-21 21:43:06 -0600

    Get to compile

--- src/app/core/password-validation.ts
+++ src/app/core/password-validation.ts
@@ -6,13 +6,11 @@ import * as zxcvbn from 'zxcvbn';
 export class PasswordValidation {
   static MatchPassword(AC: AbstractControl) {
     const password = AC.get('password').value; // to get value in input tag
-    const confirmPassword = AC.get('confirmPassword').value; // to get value in input tag
-    if(password != confirmPassword) {
+    if(password != AC.get('confirmPassword').value) {
       AC.get('confirmPassword').setErrors( {MatchPassword: true} )
     }
 
-    const passResults = zxcvbn(password);
-    if(passResults.score < 2){
+    if(zxcvbn(password).score < 2){
       AC.get('password').setErrors( {PassTooWeak: true} )
     }
     return null;
